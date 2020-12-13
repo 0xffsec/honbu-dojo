39fd70e0b894b91e755ef0eb3f52d123818ec6ef
Max Rodrigo <contact@maxrodrigo.com>
2020-12-13 11:02:09 +0100

    format containers titles

--- src/components/ContainersListItem.js
+++ src/components/ContainersListItem.js
@@ -1,4 +1,5 @@
 function ContainersListItem(props) {
+  let name = props.container.Names[0].slice(1).replace('-', ' ');
   let url;
 
   if (props.container.Ports[0] && props.container.Ports[0].PublicPort !== undefined) {
@@ -9,7 +10,7 @@ function ContainersListItem(props) {
   return (
     <tr className="hover:bg-gray-900">
       <td>
-        {props.container.Names[0]}
+        <p className="capitalize">{name}</p>
         <div className="text-sm text-gray-400">
           {props.container.Image}
         </div>
